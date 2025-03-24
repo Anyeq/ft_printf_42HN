28691dec1cfa55107fe0c19291e84c635ac8c6fe
asando <alexando17@yahoo.com>
2025-03-24 19:55:22 +0100

    draft one

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,36 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 18:22:55 by asando            #+#    #+#             */
-/*   Updated: 2025/03/24 18:22:59 by asando           ###   ########.fr       */
+/*   Updated: 2025/03/24 19:54:36 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+//%[argument$][flags][width][.precision][length modifier]conversion
+static int	parse_str(const char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\0' && !ft_isalpha(str[i]))
+		i++;
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	size_t	n_char;
+	va_list	arg_list;
+
+	i = 0;
+	n_char = 0;
+	va_start(arg_list, str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%')
+		{
+			//function to parse what is the FS and formatting
+		}
+		i++;
+	}
+	va_end(arg_list);
+	return (n_char);
+}
