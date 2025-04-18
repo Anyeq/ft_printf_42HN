b053d39cc76ff986b01c7483766a76a5a828072e
asando <alexando17@yahoo.com>
2025-04-18 08:55:26 +0200

    making ft_putint.c

--- ft_putint.c
+++ ft_putint.c
@@ -6,11 +6,13 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 14:01:30 by asando            #+#    #+#             */
-/*   Updated: 2025/04/17 16:34:30 by asando           ###   ########.fr       */
+/*   Updated: 2025/04/18 08:50:40 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
-#include "libftprintf.h"
+#include <unistd.h>
+#include <limits.h>
+//#include "libft.h"
+//#include "libftprintf.h"
 /*
  * FUNCTION (G)
  * ==> put number into fd provided
@@ -28,29 +30,53 @@
  * REFERENCE
  * ==>
 */
-void	mod_putnbr_fd(int n, int fd, t_prse *prse)
+static int	count_digit(int n)
+{
+	int	n_digit;
+
+	n_digit = 0;
+	if (n == INT_MIN)
+		return (10);
+	else if (n == 0)
+		return (1);
+	if (n < 0)
+		n = n * -1;
+	while (n > 0)
+	{
+		n = n / 10;
+		n_digit++;
+	}
+	return (n_digit);
+}
+
+static void	putint_out(int n)
 {
 	char	c;
 
 	if (n == 0)
 	{
-		write(fd, "0", 1);
+		write(1, "0", 1);
 		return ;
 	}
 	if (n == INT_MIN)
 	{
-		prse->num_min = 1;
-		write(fd, "2147483648", 11);
+		write(1, "2147483648", 10);
 		return ;
 	}
 	if (n < 0)
-	{
-		prse->num_min = 1;
 		n = n * -1;
-	}
 	if (n / 10 > 0)
-		ft_putnbr_fd(n / 10, fd);
+		putint_out(n / 10);
 	c = (n % 10) + '0';
-	write(fd, &c, 1);
+	write(1, &c, 1);
 	return ;
+}
+
+int	ft_putint(int n)
+{
+	int	n_digit;
+
+	n_digit = count_digit(n);
+	putint_out(n);
+	return (n_digit);
 }
---
+++ test_putint.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int	ft_putint(int n);
+int	main()
+{
+	printf("%d", ft_putint(0));
+	return (0);
+}
